16741ce6b9797aadda17cde549fdc3ecfe25414c
Tyler Bennett <Tyler@Tylers-MacBook-Pro.local>
2020-09-09 19:32:06 -0400

    Wrote members and most setters/getters for Class and Attribute class.

--- UML/src/Class.java
+++ UML/src/Class.java
@@ -1,45 +1,85 @@
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.Map;
+
 
 public class Class {
+    enum RelationshipType 
+    {
+        ASSOCIATION, AGGREGATION, GENERALIZATION, COMPOSITION
+    }
     private String name;
-    private ArrayList<Attribute> attributes;
-    private ArrayList<Class> aggregation;
-    private ArrayList<Class> composition;
-    private ArrayList<Class> association;
-    private ArrayList<Class> generalization;
+    //Use sets and maps so we don't have to deal with duplicates.
+    private Set<Attribute> attributes;
+    private Map<RelationshipType, Class> relationshipsToOther;
+    private Map<RelationshipType, Class> relationshipsFromOther;
 
-    //TODO: Make getters
+
+
+
+
     public String getName() {
         return this.name;
     }
 
-    public ArrayList<Attribute> getAttributes() {
+    public Set<Attribute> getAttributes() {
         return this.attributes;
     }
-    
-    public ArrayList<Class> getAggregation() {
-        return this.aggregation;
+     
+    public Map<RelationshipType, Class> getRelationshipsToOther() {
+        return this.relationshipsToOther;
     }
 
-    public ArrayList<Class> getComposition() {
-        return this.composition;
-    }
-
-    public ArrayList<Class> getAssociation() {
-        return this.association;
-    }
-    
-    public ArrayList<Class> getGeneralization() {
-        return this.generalization;
+    public Map<RelationshipType, Class> getRelationshipsFromOther() {
+        return this.relationshipsFromOther;
     }
 
 
 
 
-    //TODO: Make setters
     public void setName(String name) {
         this.name = name;
     }
+
+    public boolean addAttrubute(Attribute theAttribute) {
+        //TODO:
+        return true;
+    }
+
+    public boolean deleteAttribute(Attribute theAttribute) {
+        //TODO:
+        return true;
+    }
+
+    public boolean renameAttribute(Attribute theAttribute, String newName, String newType) {
+        //TODO:
+        return true;
+    }
+
+
+    public boolean equals(Object other) {
+        boolean result = false;
+        if(this == other) {
+            result = true;
+        }
+        else if (other == null) {
+            result = false;
+        }
+        else if(!(other instanceof Class)) { 
+            result = false; 
+        }
+        else {
+            Class object = (Class) other;
+            if(object.getName().equals(this.name) &&
+             object.getAttributes().equals(this.attributes) &&
+             object.getRelationshipsToOther().equals(this.relationshipsToOther) &&
+             object.getRelationshipsFromOther().equals(this.relationshipsFromOther)) {
+                result = true;
+             }
+        }
+        return result;
+    }
+
+
 
 
 }
