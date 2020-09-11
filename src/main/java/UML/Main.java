f4c18c5bcc48daaab900733b199efd239ed4a280
Chris <Khazarak@gmail.com>
2020-09-10 23:08:21 -0400

    Remove Main.java

--- src/main/java/UML/Main.java
+++
@@ -1,7 +0,0 @@
-public class UMLEditor
-{
-    public static void main(String[] args)
-    {
-        umlWindow uml = new umlWindow();
-    }
-}
--- src/main/java/UML/UMLEditor.java
+++ src/main/java/UML/UMLEditor.java
@@ -3,6 +3,5 @@ public class Main
     public static void main(String[] args)
     {
         umlWindow uml = new umlWindow();
-        uml.make();
     }
 }
