c0ea0600a462b880eb807097e1fd02bf83852f6f
Dominic J DuRant Russell <djdurant@floyd.cs.millersville.edu>
2020-09-10 21:54:03 -0400

    Added basic main window functionality

---
+++ src/main/java/UML/umlWindow.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+import javax.swing.*;
+public class umlWindow 
+{
+   public void window() 
+   {
+        JFrame window = new JFrame("UML");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800,750);
+        window.setVisible(true);
+   }
+
+}
