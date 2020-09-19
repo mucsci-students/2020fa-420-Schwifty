b4df96e6a1c4437216f00c9b08c46ab5bfe4d851
Chris <Khazarak@gmail.com>
2020-09-19 01:20:56 -0400

    Add load to SaveAndLoad

--- src/test/java/SaveAndLoadTest.java
+++ src/test/java/SaveAndLoadTest.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import org.junit.Test;
 import java.io.File;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 public class SaveAndLoadTest
 {
@@ -9,7 +12,9 @@ public class SaveAndLoadTest
     @Test
     /**
      * Test file: JSONTest.json
-     * Output to file: {"ClassName":"TestTwo","Attributes":"type name"}
+     * Output to file: 
+     * {"RelationshipToOthers":[],"RelationshipFromOthers":[],"ClassName":"Test","Attributes":["int num","type name"]}
+     * {"RelationshipToOthers":[],"RelationshipFromOthers":[],"ClassName":"TestTwo","Attributes":[]}
      */
     public void testSave()
     {
@@ -17,7 +22,9 @@ public class SaveAndLoadTest
         testClass.addAttribute("int", "num");
 
         Class testClassTwo = new Class("TestTwo");
-        testClass.addAttribute("type", "name");
+        testClassTwo.addAttribute("string", "aStr");
+        Class.addRelationship(testClass, testClassTwo, RelationshipType.ASSOCIATION);
+
 
         ArrayList<Class> arrList = new ArrayList<Class>();
         arrList.add(testClass);
@@ -34,6 +41,26 @@ public class SaveAndLoadTest
     @Test
     public void testSavedData()
     {
+        ArrayList<Class> classStore = new ArrayList<Class>();
+        SaveAndLoad.load("JSONTest.json", classStore);
+        String[] classNameTests = {"Test","TestTwo"};
 
+        //Create the test attributes
+        Attribute attrTest = new Attribute("num", "int");
+        Attribute attrTestTwo = new Attribute("aStr", "string");
+        Attribute[] attrTestArray = {attrTest, attrTestTwo};
+
+        int counter = 0;
+        for(Class aClass : classStore)
+        {
+            assertEquals(classNameTests[counter], aClass.getName());
+            Set<Attribute> classAttributes = aClass.getAttributes();
+            for(Attribute attr : classAttributes)
+            {
+                assertEquals(attrTestArray[counter], attr);
+            }
+            
+            counter++;
+        }
     }
 }
