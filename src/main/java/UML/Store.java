5bdb3244b6221033102cfe1df96035f6cc63a1c7
Tyler Bennett <bennett@Tylers-iMac.local>
2020-09-22 23:32:17 -0400

    Refactor/condense Menu.java and create Store.java

---
+++ src/main/java/UML/Store.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Store {
+    
+    private ArrayList<Class> classStore;
+    private File currentLoadedFile;
+
+	public Store() 
+	{
+        classStore = new ArrayList<Class>();
+        currentLoadedFile = null;
+    }
+
+    /** 
+	 * Makes and returns a combo box fill with the created classes.
+	 */
+    //Chnage name to say that it returns array of strings not classes.
+	public ArrayList<String> getClassList()
+	{
+		ArrayList<String> names = new ArrayList<String>();
+
+		for(Class aClass : classStore)
+		{
+			names.add(aClass.getName());
+		}
+		return names;
+    }
+    
+    /**
+	 * Takes in a set of attributes that are contained in a 
+	 * class and returns them as an array list of strings.
+	 */
+	public ArrayList<String> getAttributeList(Set<Attribute> attributesFromClass)
+	{
+		ArrayList<String> attributes = new ArrayList<String>();
+
+		for(Attribute attr : attributesFromClass)
+		{
+			String type = attr.getType();
+			String name = attr.getName();
+			attributes.add(type + " " + name);
+		}
+		return attributes;
+    }
+    
+    /**
+     * Removes relevant relationships when classes are deleted.
+     */
+	public void removeRelationships(Class aClass)
+	{
+		//Find the classes that the class in question has a relationship with
+		Map<String, RelationshipType> tempTo = aClass.getRelationshipsToOther();
+		Map<String, RelationshipType> tempFrom = aClass.getRelationshipsFromOther();
+		for(Map.Entry<String, RelationshipType> entry : tempTo.entrySet()) 
+		{
+			//Go to those classes and get rid of relationships to and from the class in question.
+			Class temp = findClass(entry.getKey());
+			temp.deleteRelationshipFromOther(entry.getValue(), aClass);
+		}
+		for(Map.Entry<String, RelationshipType> entry : tempFrom.entrySet()) 
+		{
+			//Go to those classes and get rid of relationships to and from the class in question.
+			Class temp = findClass(entry.getKey());
+			temp.deleteRelationshipToOther(entry.getValue(), aClass);
+		}
+    }
+    
+    /**
+	 * Finds an element in the storage and returns it. Returns null if nothing found.
+	 */
+	public Class findClass(String name)
+	{
+		for (Class aClass : classStore) 
+		{
+			if(aClass.getName().equals(name))
+			{
+				return aClass;
+			}
+		}
+		return null;
+	}
+
+}
